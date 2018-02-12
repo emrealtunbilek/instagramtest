18be995f6c6ee74d798ac26379f6d96c6f74965f
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-13 02:58:04 +0300

    profil sayfası tamamlandı gibi, gridview var ama eleman olmadıgı için görünmüyor

--- app/src/main/java/com/emrealtunbilek/instagramtest/Profile/ProfileActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Profile/ProfileActivity.kt
@@ -20,7 +20,7 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-       // setupBottomNavigationView()
+        setupBottomNavigationView()
 
         setupToolbar()
     }
