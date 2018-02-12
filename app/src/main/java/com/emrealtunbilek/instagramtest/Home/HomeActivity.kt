f239a27041f85acd9ee286218010c552d8bf008e
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-12 06:15:00 +0300

    tablarımızı olusturduk, home activitye viewpager ve tabları ekledik, bunun için pageradapter yaptık, buna fragmentlerimizi ekliyoruz ve viewpager da eklediğimiz fragmentleri göstermeye baslıyor.. ve bunu kullanarak tablarımızı da olusturuyoruz

--- app/src/main/java/com/emrealtunbilek/instagramtest/Home/HomeActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Home/HomeActivity.kt
@@ -2,10 +2,13 @@ package com.emrealtunbilek.instagramtest.Home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.util.Log
 import com.emrealtunbilek.instagramtest.R
 import com.emrealtunbilek.instagramtest.utils.BottomNavigationViewHelper
 import kotlinx.android.synthetic.main.layout_bottom_navigation_view.*
+import kotlinx.android.synthetic.main.layout_center_viewpager.*
+import kotlinx.android.synthetic.main.layout_top_tabs.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -16,7 +19,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         setupBottomNavigationView()
+
+        setupViewPager()
     }
 
 
@@ -27,5 +33,23 @@ class HomeActivity : AppCompatActivity() {
         var menu=bottomNavViewBar.menu
         var menuItem=menu.getItem(ACTIVITY_NO)
         menuItem.setChecked(true)
+    }
+
+
+    fun setupViewPager(){
+        var sectionViewPagerAdapter=SectionsPagerAdapter(supportFragmentManager)
+        sectionViewPagerAdapter.addFragment(CameraFragment())
+        sectionViewPagerAdapter.addFragment(HomeFragment())
+        sectionViewPagerAdapter.addFragment(MessageFragment())
+
+        viewPager.adapter=sectionViewPagerAdapter
+
+        tabs.setupWithViewPager(viewPager)
+
+        tabs.getTabAt(0)?.setIcon(R.drawable.ic_camera)
+        tabs.getTabAt(1)?.setIcon(R.drawable.logo)
+        tabs.getTabAt(2)?.setIcon(R.drawable.ic_arrow)
+
+        viewPager.setCurrentItem(1)
     }
 }
---
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Home/SectionsPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.emrealtunbilek.instagramtest.Home
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+/**
+ * Created by Emre on 12.02.2018.
+ */
+class SectionsPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+
+    private var mFragmentList: ArrayList<Fragment> = ArrayList<Fragment>()
+
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList.get(position)
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment){
+        mFragmentList.add(fragment)
+    }
+}
