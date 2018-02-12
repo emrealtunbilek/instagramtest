79c1bcfeffd8ad8062e5ff5641deceadf5353c63
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-12 07:05:43 +0300

    profil activity ekranını olusturmaya basladık, toolbar ekledik ve bu toolbarda menuyu olusturduk, şimdillik birşey yapmıyor ama normalde tıklayınca yeni bi activitynin olusturulması gerekiyor

--- app/src/main/java/com/emrealtunbilek/instagramtest/Profile/ProfileActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Profile/ProfileActivity.kt
@@ -2,10 +2,15 @@ package com.emrealtunbilek.instagramtest.Profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
 import com.emrealtunbilek.instagramtest.R
 import com.emrealtunbilek.instagramtest.utils.BottomNavigationViewHelper
 import kotlinx.android.synthetic.main.layout_bottom_navigation_view.*
+import kotlinx.android.synthetic.main.snippet_top_profilebar.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -14,8 +19,10 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        setupBottomNavigationView()
+        setContentView(R.layout.activity_profile)
+       // setupBottomNavigationView()
+
+        setupToolbar()
     }
 
 
@@ -26,5 +33,26 @@ class ProfileActivity : AppCompatActivity() {
         var menu=bottomNavViewBar.menu
         var menuItem=menu.getItem(ACTIVITY_NO)
         menuItem.setChecked(true)
+    }
+
+    fun setupToolbar(){
+        profileToolbar.setOnMenuItemClickListener(object : Toolbar.OnMenuItemClickListener{
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                when(item?.itemId){
+                    R.id.profileMenu -> {
+                        return true
+                    }
+                }
+                return false
+            }
+
+        })
+
+        setSupportActionBar(profileToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profile_menu,menu)
+        return true
     }
 }
