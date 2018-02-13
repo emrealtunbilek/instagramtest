c6f6bfbffb03da8c00540b5ec86f2a0fcf043352
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-13 03:25:52 +0300

    profil ayarlarında liste ile tüm ayarları listeleyebiliyoruz, geri okuna basıldıgında activity sonlanıyor

--- app/src/main/java/com/emrealtunbilek/instagramtest/Profile/AccountSettingsActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Profile/AccountSettingsActivity.kt
@@ -2,12 +2,27 @@ package com.emrealtunbilek.instagramtest.Profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.emrealtunbilek.instagramtest.R
+import kotlinx.android.synthetic.main.layout_center_accountsettings.*
+import kotlinx.android.synthetic.main.snippet_top_accountsettingtoolbar.*
 
 class AccountSettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accountsettings)
+
+        setupSettingsList()
+
+        backArrow.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun setupSettingsList(){
+        var adapter=ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, resources.getStringArray(R.array.edit_profile_options))
+        lvAccountSettings.adapter=adapter
+
     }
 }
