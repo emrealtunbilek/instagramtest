fa8f378e9ebb8033b879d2a1342e946a47b138d7
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-15 20:33:19 +0300

    firebase kullanıcı kontrolü yapılıyor eger kullanıcı login olmamıssa login ekranına gönderiliyor

--- app/src/main/java/com/emrealtunbilek/instagramtest/Home/HomeActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Home/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.emrealtunbilek.instagramtest.Home
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.emrealtunbilek.instagramtest.Login.LoginActivity
 import com.emrealtunbilek.instagramtest.R
 import com.emrealtunbilek.instagramtest.utils.BottomNavigationViewHelper
 import com.emrealtunbilek.instagramtest.utils.SectionsPagerAdapter
@@ -11,16 +13,24 @@ import com.nostra13.universalimageloader.core.ImageLoader
 import kotlinx.android.synthetic.main.layout_bottom_navigation_view.*
 import kotlinx.android.synthetic.main.layout_center_viewpager.*
 import kotlinx.android.synthetic.main.layout_top_tabs.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class HomeActivity : AppCompatActivity() {
 
-   private val TAG = "HomeActivity";
-   private val ACTIVITY_NO = 0;
+    private val TAG = "HomeActivity";
+    private val ACTIVITY_NO = 0;
 
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setupFirebaseAuth()
+
         initImageLoader()
         setupBottomNavigationView()
 
@@ -28,23 +38,23 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
-    fun setupBottomNavigationView(){
+    fun setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: bottom navigation ayarlanıyor");
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavViewBar)
         BottomNavigationViewHelper.enableNavigation(this, bottomNavViewBar)
-        var menu=bottomNavViewBar.menu
-        var menuItem=menu.getItem(ACTIVITY_NO)
+        var menu = bottomNavViewBar.menu
+        var menuItem = menu.getItem(ACTIVITY_NO)
         menuItem.setChecked(true)
     }
 
 
-    fun setupViewPager(){
-        var sectionViewPagerAdapter= SectionsPagerAdapter(supportFragmentManager)
+    fun setupViewPager() {
+        var sectionViewPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         sectionViewPagerAdapter.addFragment(CameraFragment())
         sectionViewPagerAdapter.addFragment(HomeFragment())
         sectionViewPagerAdapter.addFragment(MessageFragment())
 
-        viewPager.adapter=sectionViewPagerAdapter
+        viewPager.adapter = sectionViewPagerAdapter
 
         tabs.setupWithViewPager(viewPager)
 
@@ -55,8 +65,45 @@ class HomeActivity : AppCompatActivity() {
         viewPager.setCurrentItem(1)
     }
 
-    fun initImageLoader(){
-        var universalImageLoader= UniversalImageLoader(applicationContext)
+    fun initImageLoader() {
+        var universalImageLoader = UniversalImageLoader(applicationContext)
         ImageLoader.getInstance().init(universalImageLoader.getConfig())
+    }
+
+    fun setupFirebaseAuth() {
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = object : FirebaseAuth.AuthStateListener {
+            override fun onAuthStateChanged(p0: FirebaseAuth) {
+
+                var user = p0?.currentUser
+                checkCurrentUser(user)
+
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged: Kullanıcı bağlı:" + user.uid);
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: Kullanıcı bağlı değil");
+                }
+            }
+        }
+    }
+
+    fun checkCurrentUser(user:FirebaseUser?){
+        if(user == null){
+            val intent=Intent(this@HomeActivity,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth?.addAuthStateListener(mAuthListener!!)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mAuthListener != null) {
+            mAuth?.removeAuthStateListener(mAuthListener!!)
+        }
     }
 }
