288d525f711f139085f06aadd931e575d3a754b5
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-15 19:59:14 +0300

    firebase eklendi giriş ve kayıt ekranları olusturuldu, artık firebase kodlarını yazabilirik

---
+++ app/src/main/java/com/emrealtunbilek/instagramtest/Login/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.emrealtunbilek.instagramtest.Login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.emrealtunbilek.instagramtest.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
